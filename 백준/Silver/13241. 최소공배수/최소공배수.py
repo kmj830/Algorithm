8cfdb69123353446bdb317a9b4cfbae38cb4c70b
MinJungKim <113003936+Clavexin@users.noreply.github.com>
2023-07-01 16:47:47 +0900

    [Silver V] Title: 최소공배수, Time: 224 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -0,0 +1,15 @@
+def gcd(nums):
+    a=max(nums)
+    b=min(nums)
+    top=0
+    for i in range(1,a+1):
+        if a%i==0 and b%i==0:
+            top=i
+    return top
+def lcm(nums):
+    a=max(nums)
+    b=min(nums)
+    return a*b//gcd(nums)
+
+nums=list(map(int,input().split()))
+print(lcm(nums))
