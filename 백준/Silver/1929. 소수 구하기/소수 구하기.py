dd316483669b206c718dc23cbe238bf087506804
MinJungKim <113003936+Clavexin@users.noreply.github.com>
2023-07-02 17:23:22 +0900

    [Silver III] Title: 소수 구하기, Time: 408 ms, Memory: 46604 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,11 +1,12 @@
-def is_prime(n):
-    if n<=1:
-        return False
-    for i in range(2,int(n**0.5)+1):
-        if n%i==0:
-            return False
-    return True
-a,b=map(int,input().split())
-for i in range(a,b+1):
-    if is_prime(i):
-        print(i)
+m,n=map(int,input().split())
+
+a=[False,False]+[True]*(n-1)
+primes=[]
+
+for i in range(2,n+1):
+    if a[i]:
+        primes.append(i) if i>=m else None
+        for j in range(2*i,n+1,i):
+            a[j]=False
+for i in primes:
+    print(i)
