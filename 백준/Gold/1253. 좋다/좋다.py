0389b425d6506b5d67d930e86fef0695769fd0df
MinJung Kim <113003936+Clavexin@users.noreply.github.com>
2023-08-14 15:28:17 +0900

    [Gold IV] Title: 좋다, Time: 1080 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -1,23 +1,20 @@
-import sys
-input=sys.stdin.readline
-n=int(input())
+N=int(input())
 A=list(map(int,input().split()))
 A.sort()
 count=0
-for num in range(n):
-    find=A[num]
-    i=0;j=n-1
-    while i<j:
-        if A[i]+A[j]==find:
-            if i!=num and j!=num:
+for i in range(N):
+    start,end=0,N-1
+    while start<end:
+        if A[start]+A[end]>A[i]:
+            end-=1
+        elif A[start]+A[end]<A[i]:
+            start+=1
+        else:
+            if start==i:
+                start+=1
+            elif end==i:
+                end-=1
+            else:
                 count+=1
                 break
-            elif i==num:
-                i+=1
-            else:
-                j-=1
-        elif A[i]+A[j]>find:
-            j-=1
-        else:
-            i+=1
 print(count)
