1c0689bf69dc1128d7d2fc1c33d75776f9e2c800
MinJung Kim <113003936+Clavexin@users.noreply.github.com>
2023-08-16 18:37:16 +0900

    [Silver I] Title: 회의실 배정, Time: 292 ms, Memory: 59204 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -4,16 +4,13 @@ N=int(input())
 meetings=[]
 for _ in range(N):
     meetings.append(list(map(int,input().split())))
-meetings.sort(key=lambda x:(x[1],x[0]))
+    if meetings[-1][0]>meetings[-1][1]:
+        meetings.pop()
+meetings.sort(key=lambda x:(x[1],x[0]),reverse=True)
 count=0
-lastend=0
-# while meetings:
-#     now=meetings.pop()
-#     while meetings and now[1]>meetings[-1][0]:
-#         meetings.pop()
-#     count+=1
-for times in meetings:
-    if times[0]>=lastend:
-        count+=1
-        lastend=times[1]
+while meetings:
+    now=meetings.pop()
+    while meetings and now[1]>meetings[-1][0]:
+        meetings.pop()
+    count+=1
 print(count)
