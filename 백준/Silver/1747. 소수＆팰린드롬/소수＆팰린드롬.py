e7bcb034ed714b32fbeb7ceb60e71387f25b192e
MinJung Kim <113003936+Clavexin@users.noreply.github.com>
2023-08-17 12:03:24 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 2100 ms, Memory: 359504 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -0,0 +1,19 @@
+import sys
+input=sys.stdin.readline
+n=int(input())
+array=[False,False]+[True]*(9999999)
+for i in range(2,10000000):
+    if array[i]:
+        for j in range(i*2,10000000,i):
+            array[j]=False
+while True:
+    temp=str(n)
+    length=len(temp)
+    if array[n]:
+        for i in range(length//2):
+            if temp[i]!=temp[-i-1]:
+                break
+        else:
+            print(n)
+            break
+    n+=1
