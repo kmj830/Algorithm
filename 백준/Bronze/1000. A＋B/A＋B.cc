9604cf8babc916ebd9f4b01325d934df586bca9c
MinJung Kim <113003936+Clavexin@users.noreply.github.com>
2023-08-28 15:20:29 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,16 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
 int main()
 {
-	int A;
-	//printf("A에 넣을 숫자를 입력하세요\n");
-	scanf("%d", &A);
-	int B;
-	//printf(" B에 넣을 숫자를 입력하세요\n");
-	scanf("%d", &B);
-	
-	if (A>0 && B<10)
-	{
-		printf("%d", A + B);
-	}
+	int num1, num2;
+	scanf("%d %d",&num1,&num2);
+	printf("%d",num1+num2);
+	return 0;
 }
