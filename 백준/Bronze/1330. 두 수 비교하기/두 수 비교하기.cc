f1596f8b46500e1e4712ed194cf9fbdc454f5d2b
MinJung Kim <113003936+Clavexin@users.noreply.github.com>
2023-08-28 15:25:09 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int num1, num2;
+	scanf("%d %d",&num1,&num2);
+	if (num1 > num2) {
+		printf(">");
+	}
+	else if (num1 == num2) {
+		printf("==");
+	}
+	else {
+		printf("<");
+	}
+	
+	return 0;
+}
