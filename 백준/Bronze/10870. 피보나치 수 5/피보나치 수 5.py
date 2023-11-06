d9a2ce27be10de7e7bb6e7f4346311d6a14bb504
MinJung Kim <113003936+Clavexin@users.noreply.github.com>
2023-11-06 17:22:49 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,7 +1,8 @@
 def fibonacci(n):
     if n==0:
         return 0
-    elif n==1:
+    elif n==1 or n==2:
         return 1
-    return fibonacci(n-1)+fibonacci(n-2)
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 print(fibonacci(int(input())))
