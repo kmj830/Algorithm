649861fe75c454fcc2d08666444dec70c88ecc22
MinJung Kim <113003936+kmj830@users.noreply.github.com>
2024-06-10 22:13:14 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,18 +1,18 @@
 import sys
+input=sys.stdin.readline
+def vps(ps):
+  s=[]
+  for i in ps:
+    s.append(i)
+    try:
+      if s[-1]==')' and s[-2]=='(':
+        s.pop()
+        s.pop()
+    except:
+      None
+  if len(s)==0:
+    return 'YES'
+  else:
+    return 'NO'
 for _ in range(int(input())):
-    temp=input()
-    stack=[]
-    for i in temp:
-        if i == "(":
-            stack.append(i)
-        else:
-            try:
-                stack.pop()
-            except:
-                print("NO")
-                break
-    else:
-        if not stack:
-            print("YES")
-        else:
-            print("NO")
+  print(vps(input().strip()))
