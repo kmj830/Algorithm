77bf16dec30eb9257ee83dcd3a6f2e6099dae304
MinJung Kim <113003936+kmj830@users.noreply.github.com>
2024-06-10 22:26:18 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -7,10 +7,7 @@ def vps(ps):
     if i=='(' or i==')' or i=='[' or i==']':
       s.append(i)
     try:
-      if s[-1]==')' and s[-2]=='(':
-        s.pop()
-        s.pop()
-      elif s[-1]==']' and s[-2]=='[':
+      if (s[-1]==')' and s[-2]=='(') or (s[-1]==']' and s[-2]=='['):
         s.pop()
         s.pop()
     except:
