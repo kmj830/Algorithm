8555730082eaa54be3b68e702e5c1d8bf2dac437
MinJung Kim <113003936+kmj830@users.noreply.github.com>
2024-11-18 19:32:24 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1008 ms, Memory: 105948 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,13 +1,23 @@
+# 문제 4 / 구간 합 구하기 2
+# https://www.acmicpc.net/problem/11660
+
 import sys
 input=sys.stdin.readline
-n,m=map(int,input().split())
-a=[[0]*(n+1)]
-for _ in range(n):
-    a.append([0]+list(map(int,input().split())))
-d=[[0 for _ in range(n+1)] for _ in range(n+1)]
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        d[i][j]=d[i][j-1]+d[i-1][j]-d[i-1][j-1]+a[i][j]
-for _ in range(m):
-    x1,y1,x2,y2=map(int,input().split())
-    print(d[x2][y2]-d[x2][y1-1]-d[x1-1][y2]+d[x1-1][y1-1])
+
+# 입력
+
+N,M=map(int,input().split())
+table=[[0]*(N+1)]
+for _ in range(N):
+  table.append([0]+list(map(int,input().split())))
+
+# 전처리
+table_prcd=[[0 for _ in range(N+1)]for _ in range(N+1)]
+for i in range(1,N+1):
+  for j in range(1,N+1):
+    table_prcd[i][j]=table_prcd[i-1][j]+table_prcd[i][j-1]-table_prcd[i-1][j-1]+table[i][j]
+
+# 도출
+for _ in range(M):
+  x1,y1,x2,y2=map(int,input().split())
+  print(table_prcd[x2][y2]-table_prcd[x1-1][y2]-table_prcd[x2][y1-1]+table_prcd[x1-1][y1-1])
