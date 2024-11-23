de52276e16008beb472467a0a46c7d3b9a070920
MinJung Kim <113003936+kmj830@users.noreply.github.com>
2024-11-23 20:11:23 +0900

    [Silver I] Title: 절댓값 힙, Time: 120 ms, Memory: 39824 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,19 +1,16 @@
-import sys
-from queue import PriorityQueue
-input=sys.stdin.readline
-n=int(input())
-que=PriorityQueue()
-answer=[];count=0
-for _ in range(n):
-    x=int(input())
-    if x!=0:
-        que.put((abs(x),x))
-        count+=1
-    else:
-        if count>0:
-            answer.append(que.get()[1])
-            count-=1
-        else:
-            answer.append(0)
-for i in answer:
-    print(i)
+# 문제 14 / 절댓값 힙 구현하기
+# https://www.acmicpc.net/problem/11286
+from sys import stdin,stdout
+input=stdin.readline
+print=stdout.write
+import heapq
+a=[]
+for _ in range(int(input())):
+  ipt=int(input())
+  if ipt==0:
+    try:
+      print(f"{heapq.heappop(a)[1]}\n")
+    except:
+      print('0\n')
+  else:
+    heapq.heappush(a,(abs(ipt),ipt))
