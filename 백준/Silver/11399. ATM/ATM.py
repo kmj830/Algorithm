b0143d86b442501e53e4fdbbfd09c3b531c326f0
MinJung Kim <113003936+kmj830@users.noreply.github.com>
2024-11-24 19:38:27 +0900

    [Silver IV] Title: ATM, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,9 +1,24 @@
-import sys
-input=sys.stdin.readline
+from sys import stdin
+input=stdin.readline
+def insertion_sort(arr):
+  l=len(arr)
+  for idx in range(1,l):
+    find_idx=idx
+    for i in range(idx-1,-1,-1):
+      if arr[i]>arr[idx]:
+        find_idx=i
+    tmp=arr[idx]
+    for i in range(idx-1,find_idx-1,-1):
+      arr[i+1]=arr[i]
+    arr[find_idx]=tmp
+  return arr
+    
+
 n=int(input())
-line=list(map(int,input().split()))
-line.sort()
-total=0
-for i in range(1,len(line)+1):
-    total+=sum(line[0:i])
+a=list(map(int,input().split()))
+sorted_a=insertion_sort(a)
+total=0;num=0
+for i in sorted_a:
+  num+=i
+  total+=num
 print(total)
