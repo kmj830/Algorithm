43ebddd0f47fa56b623c21b042ffa04336e5fb2a
MinJung Kim <113003936+kmj830@users.noreply.github.com>
2024-11-24 19:06:28 +0900

    [Silver V] Title: 소트인사이드, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,3 +1,17 @@
-n=list(map(int,input()))
-n.sort(reverse=True)
-print(*n,sep="")
+def selection_sort(arr):
+  l=len(arr)
+  idx=0
+  while idx<l-1:
+    idx_max=idx
+    for i in range(idx,l):
+      if arr[idx_max]<=arr[i]:
+        idx_max=i
+    tmp=arr[idx]
+    arr[idx]=arr[idx_max]
+    arr[idx_max]=tmp
+    idx+=1
+  return arr
+
+
+a=list(map(int,input()))
+print(''.join(list(map(str,selection_sort(a)))))
