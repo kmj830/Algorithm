b869e9214d22be1e59db38204c4e26b596d83fc6
MinJung Kim <113003936+kmj830@users.noreply.github.com>
2024-11-27 19:09:47 +0900

    [Platinum V] Title: 버블 소트, Time: 2304 ms, Memory: 86564 KB -BaekjoonHub

--- 백준/Platinum/1517. 버블 소트/버블 소트.py
+++ 백준/Platinum/1517. 버블 소트/버블 소트.py
@@ -1,8 +1,11 @@
-import sys
-input=sys.stdin.readline
-result=0
+# 문제 21 / 버블 정렬 프로그램2
+# https://www.acmicpc.net/problem/1517
+
+from sys import stdin
+input=stdin.readline
+
 def merge_sort(s,e):
-  global result
+  global sc
   if e-s<1: return
   m=int(s+(e-s)/2)
   merge_sort(s,m)
@@ -10,30 +13,36 @@ def merge_sort(s,e):
   for i in range(s,e+1):
     tmp[i]=a[i]
   k=s
-  index1=s
-  index2=m+1
-  while index1<=m and index2<=e:
-    if tmp[index1]>tmp[index2]:
-      result+=index2-k
-      a[k]=tmp[index2]
+  idx1=s
+  idx2=m+1
+  while idx1<=m and idx2<=e:
+    if tmp[idx1]<=tmp[idx2]:
+      a[k]=tmp[idx1]
+      if idx1>k:
+        sc+=(idx1-k)
       k+=1
-      index2+=1
+      idx1+=1
     else:
-      a[k]=tmp[index1]
+      a[k]=tmp[idx2]
+      if idx2>k:
+        sc+=(idx2-k)
       k+=1
-      index1+=1
-  while index1<=m:
-    a[k]=tmp[index1]
+      idx2+=1
+  while idx1<=m:
+    a[k]=tmp[idx1]
+    if idx1>k:
+      sc+=(idx1-k)
     k+=1
-    index1+=1
-  while index2<=e:
-    a[k]=tmp[index2]
+    idx1+=1
+  while idx2<=m:
+    a[k]=tmp[idx2]
+    if idx2>k:
+      sc+=(idx2-k)
     k+=1
-    index2+=1
-  
-
+    idx2+=1
 n=int(input())
-a=[0]+list(map(int,input().split()))
-tmp=[0]*(n+1)
-merge_sort(1,n)
-print(result)
+a=list(map(int,input().split()))
+tmp=[0]*(n)
+sc=0
+merge_sort(0,n-1)
+print(sc)
