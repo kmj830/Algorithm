ad11b9209fda9c525e189b9e643cca5550f5d908
MinJung Kim <113003936+kmj830@users.noreply.github.com>
2026-03-05 18:11:09 +0900

    [Silver IV] Title: 대칭 차집합, Time: 672 ms, Memory: 90520 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.stream.Collectors;
@@ -9,20 +8,16 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         br.readLine();
-
-
+        int count = 0;
         HashSet<Integer> a = Arrays.stream(br.readLine().split(" ")).map(Integer::valueOf).collect(Collectors.toCollection(HashSet::new));
         HashSet<Integer> b = Arrays.stream(br.readLine().split(" ")).map(Integer::valueOf).collect(Collectors.toCollection(HashSet::new));
 
-        HashSet<Integer> temp = (HashSet<Integer>) a.clone();
-
-        a.retainAll(b);
-
-        temp.addAll(b);
-        temp.removeAll(a);
-
-        System.out.println(temp.size());
+        for (int i : a) {
+            if (b.contains(i)) {
+                count++;
+            }
+        }
+        System.out.println(a.size() + b.size() - 2 * count);
     }
 }
