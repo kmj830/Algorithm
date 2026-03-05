4900d998e574deda762eb23036d23522ec818745
MinJung Kim <113003936+kmj830@users.noreply.github.com>
2026-03-05 11:35:16 +0900

    [Silver V] Title: 숫자 카드, Time: 2748 ms, Memory: 171836 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -4,12 +4,18 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        HashSet<Integer> set = new HashSet<>(Arrays.asList(Arrays.stream(Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray()).boxed().toArray(Integer[]::new)));
+//        HashSet<Integer> set = new HashSet<>(Arrays.asList(Arrays.stream(Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray()).boxed().toArray(Integer[]::new)));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+        while (st.hasMoreTokens()) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
         int m = Integer.parseInt(br.readLine());
         int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         for (int i : array) {
