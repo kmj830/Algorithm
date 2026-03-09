1ab1d734c8fcf30e79089c4fd636944c68bd87af
MinJung Kim <113003936+kmj830@users.noreply.github.com>
2026-03-09 09:32:41 +0900

    [Silver V] Title: 최소공배수, Time: 112 ms, Memory: 14424 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        long a = input[0], b = input[1];
+        System.out.println(lcm(a,b));
+    }
+
+    private static long lcm(long a, long b) {
+        return a * b / (gcd(a, b));
+    }
+
+    private static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
