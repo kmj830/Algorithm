2003dbb693b761a068893727cb55daf6f665f373
MinJung Kim <113003936+kmj830@users.noreply.github.com>
2026-03-26 17:35:43 +0900

    [Bronze I] Title: 이항 계수 1, Time: 108 ms, Memory: 14468 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] inputArr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        System.out.println(factorial(inputArr[0])/(factorial(inputArr[1])*factorial(inputArr[0]-inputArr[1])));
+    }
+
+    private static int factorial(int n) {
+        int result = 1;
+        for (int i = n; i > 0; i--) {
+            result *= i;
+        }
+        return result;
+    }
+}
